3fd1510c6e70ab9ac64f8f21ec00ca16f0f2e26c
Swati Krishnan <skrishnan@zendesk.com>
2019-02-12 15:02:31 -0800

    more

--- lib/gem/diff/command.rb
+++ lib/gem/diff/command.rb
@@ -3,8 +3,13 @@ require 'rubygems/command'
 module Gem
   module Diff
     class Command < ::Gem::Command
-      def self.invoke_with_build_args(a, b)
-        puts "A #{a} #{b}"
+      def self.invoke_with_build_args(argv, _build_argv)
+        gem_name = argv[0]
+        old_version = argv[1]
+        new_verison = argv[2]
+        old_gem = Gem::Specification.find_by_name gem_name, old_version
+        new_gem = Gem::Specification.find_by_name gem_name, new_version
+        exec 'git', 'diff', old_gem.gem_dir, new_gem.gem_dir
       end
 
       def self.summary
--- test/gem/diff/command_test.rb
+++ test/gem/diff/command_test.rb
@@ -3,10 +3,38 @@ require_relative "../../test_helper"
 
 SingleCov.covered!
 
+# exec would kill our testing, so let's just do system
+Gem::Diff::Command.class_eval do
+  def exec(*args)
+    system *args
+  end
+end
+
 describe Gem::Diff::Command do
+
+
+  def capture_stdout
+    old = $stdout
+    $stdout = StringIO.new
+    yield
+    $stdout.string
+  ensure
+    $stdout = old
+  end
+
+
   describe ".invoke_with_build_args" do
+    it "shows diff" do
+      capture_stdout do
+        Gem::Diff::Command.invoke_with_build_args(["rack", "2.0.5", "2.0.6"], []).must_equal true
+      end.must_equal "XXX"
+    end
   end
 
   describe ".summary" do
+    it "describes the command" do
+      Gem::Diff::Command.summary.must_include "diff"
+    end
   end
 end
+
